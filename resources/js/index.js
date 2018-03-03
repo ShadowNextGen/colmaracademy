727af5371f668e9001fc282e0c0451cf34565cd0
ShadowNextGen <34259179+ShadowNextGen@users.noreply.github.com>
2018-03-02 19:14:11 -0800

    Fixing Images

--- resources/js/index.js
+++ resources/js/index.js
@@ -7,14 +7,14 @@ var guestLecturesImage = document.getElementById('guest-lectures-image');
 
 window.onresize = window.onload = function () {
     if (window.innerWidth >= 800) {
-      orientationImage.src = './resources/images/information-orientation.jpg';
-      campusImage.src = './resources/images/information-campus.jpg';
-      guestLecturesImage.src = './resources/images/information-guest-lecture.jpg';
+      orientationImage.src = 'https://github.com/ShadowNextGen/colmaracademy/blob/master/resources/images/information-orientation.jpg';
+      campusImage.src = 'https://github.com/ShadowNextGen/colmaracademy/blob/master/resources/images/information-campus.jpg';
+      guestLecturesImage.src = 'https://github.com/ShadowNextGen/colmaracademy/blob/master/resources/images/information-guest-lecture.jpg';
     }
     else {
-      orientationImage.src = './resources/images/information-orientation-mobile.jpg';
-      campusImage.src = './resources/images/information-campus-mobile.jpg';
-      guestLecturesImage.src = './resources/images/information-guest-lecture-mobile.jpg';
+      orientationImage.src = 'https://github.com/ShadowNextGen/colmaracademy/blob/master/resources/images/information-orientation-mobile.jpg';
+      campusImage.src = 'https://github.com/ShadowNextGen/colmaracademy/blob/master/resources/images/information-campus-mobile.jpg';
+      guestLecturesImage.src = 'https://github.com/ShadowNextGen/colmaracademy/blob/master/resources/images/information-guest-lecture-mobile.jpg';
     }
 
 };
